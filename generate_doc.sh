cc32e22c2fc223929a8afbf70ce6a68be92a068a
Matthias Wiesenberger <Matthias.Wiesenberger@uibk.ac.at>
2015-03-16 13:49:30 +0100

    changed MathJax paths

--- generate_doc.sh
+++ generate_doc.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+#this file automatically loops over inc and src directories and runs doxygen
+# on the Doxyfiles and outputs documentation in the current directory
+# It generates tag files so that src documentation can link to inc documentations
 
-#pay attention on name collisions through tag files
+#pay attention on name collisions in docus through tag files
 include="inc/dg inc/file inc/toefl src/solovev"
 src="src/feltor src/feltor2D"
 all="$include $src"
@@ -20,6 +23,7 @@ done;
 for i in $include;
 do (cat ../$i/Doxyfile; \
 	echo "INPUT = ../$i/"; \
+	echo "MATHJAX_RELPATH = http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"; \
     echo "OUTPUT_DIRECTORY = ./$i/")  \
     | doxygen - ;
 done;
@@ -31,6 +35,7 @@ do (cat ../$i/Doxyfile; \
 	echo "OUTPUT_DIRECTORY = ./$i/"; \
     echo "EXTERNAL_GROUPS=NO" ;\
     echo "EXTERNAL_PAGES=NO" ;\
+	echo "MATHJAX_RELPATH = http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"; \
     echo "TAGFILES = $tagfiles") | doxygen - 
 done;
 
